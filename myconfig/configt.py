eaedb25de2041fc1a3c03069a668f66660d6aa88
yeaso <76080835+yeaso@users.noreply.github.com>
2021-04-05 12:41:52 +0800

    Create configt.py

---
+++ myconfig/configt.py
@@ -0,0 +1,9 @@
+BASE_PATH = "datasets/idc"
+
+TRAIN_PATH = os.path.sep.join([BASE_PATH, "train"])
+VAL_PATH = os.path.sep.join([BASE_PATH, "validation"])
+TEST_PATH = os.path.sep.join([BASE_PATH, "test"])
+
+TRAIN_SPLIT = 0.8
+
+VAL_SPLIT = 0.1
